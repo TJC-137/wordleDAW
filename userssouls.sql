9e521c0b3f0c03b8db445a3f02af54eb53e89652
Marc_Vieiras_Martorell <131770408+MarcVieirasMartorell@users.noreply.github.com>
2025-01-15 15:54:51 +0100

    Update userssouls.sql

--- userssouls.sql
+++ userssouls.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-01-2025 a las 15:48:51
+-- Tiempo de generación: 15-01-2025 a las 15:54:29
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `userssouls`
 --
+CREATE DATABASE IF NOT EXISTS `userssouls` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `userssouls`;
 
 -- --------------------------------------------------------
 
